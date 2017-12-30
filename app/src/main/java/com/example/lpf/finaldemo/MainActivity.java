6a4c9b8fe3ab88df7c788ab3daad841b2ed8904a
SMIELPF <1608272694@qq.com>
2017-12-30 13:34:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lpf/finaldemo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.lpf.finaldemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in_layout);
+    }
+}
